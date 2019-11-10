60bdbd57865d3f960b22d66b2104a676ac7b6c9a
Danielle Spencer <d.m.spencer.0486@gmail.com>
2019-11-10 14:35:58 -0600

    push exercise updates

--- exercises/part1-2.ts
+++ exercises/part1-2.ts
@@ -2,15 +2,23 @@
 // https://education.launchcode.org/intro-to-professional-web-dev/chapters/typescript/exercises.html 
 
 // Part 1: Add the 5 variables here
-
-
-
-
+let spacecraftName: string = 'Determination';
+let speedMph: number = 17500;
+let kilometersToMars: number = 225000000;
+let kilometersToTheMoon: number = 384400;
+let milesPerKilometer: number = 0.621;
 
 // Code part 2 here:
 
+//Miles
+let milesToMars: number = kilometersToMars * milesPerKilometer;
 
+//Hours
+let hoursToMars: number = milesToMars/speedMph;
 
+//Days
+let daysToMars: number = hoursToMars/24;
 
 // Code the output statement here (use a template literal):
+console.log(`${spacecraftName} would take ${daysToMars} days to get to Mars`);
 
